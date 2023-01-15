53de74026e0800047d07367fcc14a8109aa65886
Raushan Ranjan <74482501+raushanranjan103@users.noreply.github.com>
2023-01-15 11:22:46 +0530

    working code

--- mpi part 2/prgm4.c
+++ mpi part 2/prgm4.c
@@ -1,37 +1,79 @@
 #include<stdio.h>
+
 #include<stdlib.h>
+
 #include<mpi.h>
+
 //do not give high input bcos the stack will overflow and no output will be seen (segmentation fault)
+
 //try n = 7 or 8
+
 void main(int argc,char * argv[]){
+
 	int n,rank,size;
-	scanf("%d",&n);
+
 	MPI_Init(&argc,&argv);
+
 	MPI_Comm_size(MPI_COMM_WORLD,&size);
+
 	MPI_Comm_rank(MPI_COMM_WORLD,&rank);
+
 	int root=0;
-	
-	int global;	
-		//len = (n/size)+1;
-		//printf("len %d\n",len);
-	
-	MPI_Bcast(&n,1,MPI_INT,root,MPI_COMM_WORLD);
-	int localprod=1;
-	int len = (n/size)+1;
-	//printf("RANK %d\n",rank);
-	for(int i=1;i<=len;i++){
-		printf("rank %d len:%d i:%d\n n:%d",rank,len,i,n);
-		int val = (rank*len) + i;
-		printf("rank: %d val :%d\n",rank,val);
-		if(val<=n)
-		localprod *= val;
-		else break;
-	}
-	printf("LOCAL PROF:%d\n",localprod);
-MPI_Reduce(&localprod,&global,1,MPI_INT,MPI_PROD,root,MPI_COMM_WORLD);
-if(rank==root)
-		printf("Factorial of %d is %d\n",n,global);
-	MPI_Finalize();
 
 	
+
+	int global;	
+
+		//len = (n/size)+1;
+
+		//printf("len %d\n",len);
+
+	if(rank==0)
+
+	{printf("\n enter the value of n");
+
+	scanf("%d",&n);
+
+	}
+
+	
+
+	MPI_Bcast(&n,1,MPI_INT,root,MPI_COMM_WORLD);
+
+	int localprod=1;
+
+	int len = (n/size)+1;
+
+	//printf("RANK %d\n",rank);
+
+	for(int i=1;i<=len;i++){
+
+		printf("rank %d len:%d i:%d\n n:%d",rank,len,i,n);
+
+		int val = (rank*len) + i;
+
+		printf("rank: %d val :%d\n",rank,val);
+
+		if(val<=n)
+
+		localprod *= val;
+
+		else break;
+
+	}
+
+	printf("LOCAL PROF:%d\n",localprod);
+
+MPI_Reduce(&localprod,&global,1,MPI_INT,MPI_PROD,root,MPI_COMM_WORLD);
+
+if(rank==root)
+
+		printf("Factorial of %d is %d\n",n,global);
+
+	MPI_Finalize();
+
+
+
+	
+
 }
